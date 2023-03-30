e98cc522cb0c5e1ee42dfff777ea3af2d120f67e
Mahmoud Adel <112221498+Mahmoudaboutaleb@users.noreply.github.com>
2023-03-30 05:30:49 +0200

    upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './App'
+import HTML from '../src/pages/home'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: "We Are Sorry",
+  },
+  {
+    path: "/portofolio",
+    element: <HTML />,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
